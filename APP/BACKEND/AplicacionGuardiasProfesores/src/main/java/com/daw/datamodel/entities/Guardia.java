97d14e820ae13d7b958e09fb435427b556299183
mcaruisan <mcarmen.rs79@gmail.com>
2025-04-24 20:36:43 +0200

    Se añaden atributos a las entidades

--- APP/BACKEND/AplicacionGuardiasProfesores/src/main/java/com/daw/datamodel/entities/Guardia.java
+++ APP/BACKEND/AplicacionGuardiasProfesores/src/main/java/com/daw/datamodel/entities/Guardia.java
@@ -1,5 +1,27 @@
 package com.daw.datamodel.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "guardia")
 public class Guardia {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idGuardia")
+	private Long idGuardia;
+	
+	@Column(name = "grupo", nullable = false)
+	private String grupo;
+	
+	@Column(name = "duracion", nullable = false)
+	private String duracion;
 
 }
--- APP/BACKEND/AplicacionGuardiasProfesores/src/main/java/com/daw/datamodel/entities/Horario.java
+++ APP/BACKEND/AplicacionGuardiasProfesores/src/main/java/com/daw/datamodel/entities/Horario.java
@@ -1,5 +1,36 @@
 package com.daw.datamodel.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "horario")
 public class Horario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name= "numRegistro")
+	private Long numRegistro;
+	
+	@Column(name = "grupo", nullable = false)
+	private String grupo;
+	
+	@Column(name = "asignatura", nullable = false)
+	private String asignatura;
+	
+	@Column(name= "aula", nullable = true)
+	private String aula;
+	
+	@Column(name = "dia", nullable = false)
+	private Integer dia;
+	
+	@Column(name = "hora", nullable = false)
+	private Integer hora;
 
 }
