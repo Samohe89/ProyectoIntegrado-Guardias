fc809370c82fbf9421ebea7771f4a7bf285d2e79
sanmorher <smorher483@iesalmudeyne.es>
2025-04-30 22:10:57 +0200

    Diseño interfaz menú Profesor y Directivo

--- APP/FRONTEND/AppGuardias/src/app/app.component.ts
+++ APP/FRONTEND/AppGuardias/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MenuProfesorComponent } from "./menu-profesor/menu-profesor.component";
+import { MenuDirectivoComponent } from "./menu-directivo/menu-directivo.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MenuProfesorComponent, MenuDirectivoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'AppGuardias';
+
+  rol: String = "directivo";
+
+  usuarioLogueado: boolean = true;
 }
---
+++ APP/FRONTEND/AppGuardias/src/app/menu-directivo/menu-directivo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-directivo',
+  imports: [],
+  templateUrl: './menu-directivo.component.html',
+  styleUrl: './menu-directivo.component.css'
+})
+export class MenuDirectivoComponent {
+
+}
