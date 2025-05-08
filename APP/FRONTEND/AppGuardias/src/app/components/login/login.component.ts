061b3396f823c22e5b2642b37d892b530d1782a9
sanmorher <smorher483@iesalmudeyne.es>
2025-05-09 01:13:09 +0200

    Login_Frontend: Estilos CSS e interfaz terminados

--- APP/FRONTEND/AppGuardias/src/app/components/login/login.component.ts
+++ APP/FRONTEND/AppGuardias/src/app/components/login/login.component.ts
@@ -11,24 +11,36 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 
 export class LoginComponent {
   formularioLogin: FormGroup;
-  submit = false;
+  submit: Boolean = false;
+  inputType: String = "password";
   
-  @Output() loginOk = new EventEmitter<{rol:string, cursoAcademico: String}>();
+  
+
+  @Output() loginOk = new EventEmitter<{ rol: string, cursoAcademico: String }>();
 
   constructor(private form: FormBuilder) {
     this.formularioLogin = this.form.group({
-      email: ['', [Validators.required, Validators.email]],
+      usuario: ['', Validators.required],
       password: ['', Validators.required],
       rol: ['', Validators.required],
-      cursoAcademico: ['', Validators.required]
+      cursoAcademico: ['2024', Validators.required]
     });
+  }
+
+
+  mostrarPassword() {
+    this.inputType = 'text';
+  }
+
+  ocultarPassword() {
+    this.inputType = 'password'; 
   }
 
   onSubmit() {
     this.submit = true;
     if (this.formularioLogin.valid) {
-      const { email, password, rol, cursoAcademico} = this.formularioLogin.value;
-      this.loginOk.emit({rol, cursoAcademico});
+      const { email, password, rol, cursoAcademico } = this.formularioLogin.value;
+      this.loginOk.emit({ rol, cursoAcademico });
     } else {
       console.log('Error en datos introducidos');
     }
