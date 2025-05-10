9a37745b409409dea514ef5e3ed54280e8bde52c
manleofer <mleofer304@iesalmudeyne.es>
2025-05-10 09:46:36 +0200

    Creo componente de listado de ausencias de profesor.

---
+++ APP/FRONTEND/AppGuardias/src/app/listado-ausencias-profesor/listado-ausencias-profesor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-ausencias-profesor',
+  standalone: true,
+  imports: [],
+  templateUrl: './listado-ausencias-profesor.component.html',
+  styleUrl: './listado-ausencias-profesor.component.css'
+})
+export class ListadoAusenciasProfesorComponent {
+
+}
