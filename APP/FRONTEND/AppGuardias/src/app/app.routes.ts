b8b08c139cf795cfc316364f9a8ad502385989bb
sanmorher <smorher483@iesalmudeyne.es>
2025-05-15 19:41:34 +0200

    Menu_Frontend: Rutas configuradas (comentadas a falta de Componentes)

--- APP/FRONTEND/AppGuardias/src/app/app.routes.ts
+++ APP/FRONTEND/AppGuardias/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: "login", component: LoginComponent },
+    // { path: "registro-ausencia", component: RegistroAusenciaComponent },
+    // { path: "ausencias", component: ListadoAusenciasComponent },
+    // { path: "guardias", component: ListadoGuardiasComponent },
+    // { path: "horas-guardia", component: HorasGuardiaComponent },
+    
+];
