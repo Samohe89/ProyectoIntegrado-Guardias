2ed06fb6852f98edbc92ddae919a338af9beb850
manleofer <mleofer304@iesalmudeyne.es>
2025-05-17 13:11:27 +0200

    Creación del listado de mis ausencias

--- APP/FRONTEND/AppGuardias/src/app/listado-ausencias-profesor/listado-ausencias-profesor.component.ts
+++ APP/FRONTEND/AppGuardias/src/app/listado-ausencias-profesor/listado-ausencias-profesor.component.ts
@@ -7,16 +7,46 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './listado-ausencias-profesor.component.html',
-  styleUrl: './listado-ausencias-profesor.component.css'
+  styleUrl: './listado-ausencias-profesor.component.css',
 })
 export class ListadoAusenciasProfesorComponent implements OnInit {
   ausencias: Ausencia[] = [];
 
+  mostrarModal: boolean = false;
+  ausenciaSeleccionada: Ausencia | null = null;
+
   constructor(private ausenciaService: AusenciaService) {}
 
   ngOnInit() {
-    this.ausenciaService.getAll().subscribe(data => {
+    this.ausenciaService.getAll().subscribe((data) => {
       this.ausencias = data;
     });
   }
+
+  //Lógica para eliminar una ausencia
+
+  abrirModal(ausencia: Ausencia) {
+    this.ausenciaSeleccionada = ausencia;
+    this.mostrarModal = true;
+  }
+
+  cancelarEliminacion() {
+    this.ausenciaSeleccionada = null;
+    this.mostrarModal = false;
+  }
+
+  confirmarEliminacion() {
+    if (this.ausenciaSeleccionada?.id) {
+      this.ausenciaService
+        .delete(this.ausenciaSeleccionada.id)
+        .subscribe(() => {
+          // Quitar del array local
+          this.ausencias = this.ausencias.filter(
+            (a) => a.id !== this.ausenciaSeleccionada?.id
+          );
+          this.cancelarEliminacion(); // Cierra modal
+        });
+    }
+  }
+
 }
