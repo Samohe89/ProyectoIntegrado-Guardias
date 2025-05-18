6dfbe1671f74ffd972c6994b1e77b739b5a9f70f
mcaruisan <mcarmen.rs79@gmail.com>
2025-05-18 17:58:27 +0200

    Vista tabla total horas de guardia, html y css

---
+++ APP/FRONTEND/AppGuardias/src/app/components/horas-guardia/horas-guardia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horas-guardia',
+  imports: [],
+  templateUrl: './horas-guardia.component.html',
+  styleUrl: './horas-guardia.component.css'
+})
+export class HorasGuardiaComponent {
+
+}
