64a049188155d7211f99174fc5fbce005b563842
sanmorher <smorher483@iesalmudeyne.es>
2025-05-19 23:24:14 +0200

    ListadoGuardias_Frontend: El listado agrupa por horas

--- APP/FRONTEND/AppGuardias/src/app/components/listado-guardias/listado-guardias.component.ts
+++ APP/FRONTEND/AppGuardias/src/app/components/listado-guardias/listado-guardias.component.ts
@@ -23,26 +23,62 @@ export class ListadoGuardiasComponent implements OnInit {
   // Array que almacena las ausencias que envía el backend
   ausencias: any[] = [];
 
+  // Array de ausencias agrupadas por hora
+  ausenciasAgrupadas: { hora: string, ausencias: any[] }[] = [];
+
 
   constructor(private ausenciaService: AusenciaService) { }
 
   ngOnInit(): void {
     // Calcular el dia siguiente
-     this.diaSiguiente.setDate(this.diaActual.getDate() + 1);  
+    this.diaSiguiente.setDate(this.diaActual.getDate() + 1);
 
     // Cargar las ausencias del día actual al inicio
     this.cargarAusencias(this.diaActual);
   }
 
-  
+
   // Cargar las ausencias registradas en una fecha concreta
   cargarAusencias(fecha: Date): void {
+    // Actualizar la fecha mostrada
+    this.fechaMostrada = fecha;
+    
+    // Formatear fecha que sen envía al backend
     const fechaFormateada = this.formatearFecha(fecha);
+
     this.ausenciaService.getAusenciasPorFecha(fechaFormateada).subscribe({
       next: data => {
         this.ausencias = data;
-        this.fechaMostrada = fecha;  // Actualiza la fecha mostrada
         console.log(this.ausencias);
+
+        // Se resetea el array de ausencias agrupadas
+        this.ausenciasAgrupadas = [];
+
+        // Variable que almacena la hora por la que se está agrupando
+        let horaAgrupacion = null;
+
+        // Variable que contiene el índice del array de ausencias agrupadas
+        let indiceAgrupacion = -1;  // Se inicia en -1 porque está vacío, la primera agrupación sería el índice 0 del array
+
+        // Para cada ausencia enviada por el backend
+        for (let ausencia of this.ausencias) {
+          // Si la hora de la ausencia no coincide con la hora por la que se está agrupando:
+          if (ausencia.horariosProfesor.hora !== horaAgrupacion) {
+            horaAgrupacion = ausencia.horariosProfesor.hora;
+            // Se crea una nueva agrupacion en la que se incluye esta ausencia
+            this.ausenciasAgrupadas.push({
+              hora: horaAgrupacion,
+              ausencias: [ausencia]
+            });
+            // Se incrementa el contador de esta agrupación    
+            indiceAgrupacion++;
+            // Si la hora de la ausencia coincide con la hora por la que ya se está agrupando:
+          } else {
+            // Se añade la ausencia a la misma agrupación anterior (con la misma hora e índice en el array)
+            this.ausenciasAgrupadas[indiceAgrupacion].ausencias.push(ausencia);
+          }
+        }
+        console.log(this.ausenciasAgrupadas);
       },
       error: err => {
         console.error('Error al cargar las ausencias', err);
@@ -50,20 +86,18 @@ export class ListadoGuardiasComponent implements OnInit {
     });
   }
 
-esMismaFecha (fecha1: Date, fecha2: Date): boolean {
-  if (fecha1.toDateString() === fecha2.toDateString()) {
-    return true;
-  } else {
-    return false;
-  };
-}
 
-formatearFecha(fecha: Date): string {
-  return fecha.toISOString().split('T')[0]; // Se queda solo con la fecha 'YYYY-MM-DD'
-}
+  esMismaFecha(fecha1: Date, fecha2: Date): boolean {
+    if (fecha1.toDateString() === fecha2.toDateString()) {
+      return true;
+    } else {
+      return false;
+    };
+  }
 
-
- 
+  formatearFecha(fecha: Date): string {
+    return fecha.toISOString().split('T')[0]; // Se queda solo con la fecha 'YYYY-MM-DD'
+  }
 
 
 }
