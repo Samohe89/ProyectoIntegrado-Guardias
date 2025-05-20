23197bcf252ebe2c08f9b7e2f1435748f42c7cfe
mcaruisan <mcarmen.rs79@gmail.com>
2025-05-20 21:01:00 +0200

    Se añade la lógica del componente horasGuardia en angular

--- APP/FRONTEND/AppGuardias/src/app/components/horas-guardia/horas-guardia.component.ts
+++ APP/FRONTEND/AppGuardias/src/app/components/horas-guardia/horas-guardia.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HorasGuardiaService, ProfesorTotalHorasGuardiaDTO } from '../../services/horasGuardia.service';
+
 
 @Component({
   selector: 'app-horas-guardia',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './horas-guardia.component.html',
   styleUrl: './horas-guardia.component.css'
 })
-export class HorasGuardiaComponent {
+export class HorasGuardiaComponent implements OnInit{
+
+  profesoresConHoras: ProfesorTotalHorasGuardiaDTO[] = [];
+
+  constructor(private horasGuardiaService: HorasGuardiaService) {}
+
+  ngOnInit(): void {
+    this.horasGuardiaService.getTotalHorasPorProfesor()
+      .subscribe({
+        next: (data) => this.profesoresConHoras = data,
+        error: (err) => console.error('Error al obtener las horas de guardia:', err)
+      });
+  }
+
 
 }
