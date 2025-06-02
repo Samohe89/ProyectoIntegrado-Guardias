000986fae9edc77b05ff5812c5cb1ff2361a9387
sanmorher <smorher483@iesalmudeyne.es>
2025-06-03 00:34:07 +0200

    Filtrado_Frontend: Se implementa validación de criterios

--- APP/FRONTEND/AppGuardias/src/app/components/filtrado/filtrado.component.ts
+++ APP/FRONTEND/AppGuardias/src/app/components/filtrado/filtrado.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Profesor } from '../../services/ausencia.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ProfesorService } from '../../services/profesor.service';
 
 @Component({
@@ -21,6 +21,13 @@ export class FiltradoComponent {
   fechaHasta: string = '';
   profesorFiltro: string | null = null;
 
+
+  // Variables de validación de criterios de filtrado
+  faltanCriterios: boolean = false;
+  faltanFechas: boolean = false;
+  fechasErroneas: boolean = false;
+
+
   // Evento que se emitirá al componente padre, enviando las variables de filtrado
   @Output() filtrosAplicados = new EventEmitter<{
     fechaDesde: string;
@@ -28,17 +35,38 @@ export class FiltradoComponent {
     profesorFiltro: string | null;
   }>();
 
-  constructor(
-      private profesorService: ProfesorService
-    ) { }
 
-    
+
+  constructor(
+    private profesorService: ProfesorService
+  ) { }
+
+
   ngOnInit(): void {
     this.profesorService.getProfesores().subscribe({
       next: profesores => this.profesores = profesores,
       error: err => console.error('Error al cargar profesores:', err)
     });
   }
+
+
+  onSubmit(form: NgForm): void {
+    this.faltanCriterios = false;
+    this.faltanFechas = false;
+    this.fechasErroneas = false;
+
+    if (!this.fechaDesde && !this.fechaHasta && this.profesorFiltro == null) {
+      this.faltanCriterios = true;
+    } else if ((this.fechaDesde && !this.fechaHasta) || (!this.fechaDesde && this.fechaHasta)) {
+      this.faltanFechas = true;
+    } else if (this.fechaDesde && this.fechaHasta && this.fechaDesde > this.fechaHasta) {
+      this.fechasErroneas = true;
+    } else {
+      this.aplicarFiltros();
+    }
+
+  }
+
 
   // Método que aplica el valor de las variables de filtrado
   aplicarFiltros() {
@@ -54,5 +82,17 @@ export class FiltradoComponent {
     this.fechaHasta = '';
     this.profesor= '';
     */
+  }
+  
+  // Método para resetear las variables de validacion
+  resetValidacion(): void {
+    if (this.fechaDesde || this.fechaHasta) {
+      this.faltanFechas = false;
+      this.fechasErroneas = false;
+    }
+
+    if (this.fechaDesde || this.fechaHasta || this.profesorFiltro) {
+      this.faltanCriterios = false;
+    }
   }
 }
