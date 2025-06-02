61d655446c737fd89656b4f3485cd2155889d112
manleofer <mleofer304@iesalmudeyne.es>
2025-06-02 13:19:13 +0200

    Creación de la rama de filtrado, y lógica de la misma para el listado de ausencias.

---
+++ APP/FRONTEND/AppGuardias/src/app/components/filtrado/filtrado.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Profesor } from '../../services/ausencia.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-filtrado',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './filtrado.component.html',
+})
+export class FiltroAusenciasComponent {
+  @Input() profesores: Profesor[] = [];
+
+  fechaDesde: string = '';
+  fechaHasta: string = '';
+  profesorFiltro: string = '';
+
+  @Output() filtrosAplicados = new EventEmitter<{
+    fechaDesde: string;
+    fechaHasta: string;
+    profesor: string;
+  }>();
+
+  aplicarFiltros() {
+    this.filtrosAplicados.emit({
+      fechaDesde: this.fechaDesde,
+      fechaHasta: this.fechaHasta,
+      profesor: this.profesorFiltro,
+    });
+
+    this.fechaDesde = '';
+    this.fechaHasta = '';
+    this.profesorFiltro = '';
+  }
+}
