42bbbb9672d2788d443d7f7620a7d0571a946944
sanmorher <smorher483@iesalmudeyne.es>
2025-06-02 21:53:29 +0200

    Filtrado_Frontend: Se acualiza lógica de filtrado y carga de select profesor

--- APP/FRONTEND/AppGuardias/src/app/components/filtrado/filtrado.component.ts
+++ APP/FRONTEND/AppGuardias/src/app/components/filtrado/filtrado.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Profesor } from '../../services/ausencia.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ProfesorService } from '../../services/profesor.service';
 
 @Component({
   selector: 'app-filtrado',
@@ -12,27 +13,46 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class FiltradoComponent {
-  @Input() profesores: Profesor[] = [];
+  // Array de profesores para mostrar en el select
+  profesores: Profesor[] = [];
 
+  // Variables del formulario de filtrado
   fechaDesde: string = '';
   fechaHasta: string = '';
-  profesorFiltro: string = '';
+  profesorFiltro: string | null = null;
 
+  // Evento que se emitirá al componente padre, enviando las variables de filtrado
   @Output() filtrosAplicados = new EventEmitter<{
     fechaDesde: string;
     fechaHasta: string;
-    profesor: string;
+    profesorFiltro: string | null;
   }>();
 
+  constructor(
+      private profesorService: ProfesorService
+    ) { }
+
+    
+  ngOnInit(): void {
+    this.profesorService.getProfesores().subscribe({
+      next: profesores => this.profesores = profesores,
+      error: err => console.error('Error al cargar profesores:', err)
+    });
+  }
+
+  // Método que aplica el valor de las variables de filtrado
   aplicarFiltros() {
     this.filtrosAplicados.emit({
       fechaDesde: this.fechaDesde,
       fechaHasta: this.fechaHasta,
-      profesor: this.profesorFiltro,
+      profesorFiltro: this.profesorFiltro,
     });
 
+    // Posible limpieza del formulario de filtrado
+    /*
     this.fechaDesde = '';
     this.fechaHasta = '';
-    this.profesorFiltro = '';
+    this.profesor= '';
+    */
   }
 }
