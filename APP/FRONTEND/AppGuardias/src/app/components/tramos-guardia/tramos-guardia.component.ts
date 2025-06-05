a59c52996df68a4c0187006b348979d4c24ac4d8
sanmorher <smorher483@iesalmudeyne.es>
2025-06-05 19:33:34 +0200

    RegistroGuardia_Frontend: Estilos y apariencia Directivo

--- APP/FRONTEND/AppGuardias/src/app/components/tramos-guardia/tramos-guardia.component.ts
+++ APP/FRONTEND/AppGuardias/src/app/components/tramos-guardia/tramos-guardia.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Profesor, ProfesorService } from '../../services/profesor.service';
 
 @Component({
   selector: 'app-tramos-guardia',
@@ -11,8 +12,18 @@ import { CommonModule } from '@angular/common';
 })
 
 export class TramosGuardiaComponent {
+  
+   // Variable que almacena los datos del usuario que tiene abierta la sesión
+   usuario = JSON.parse(sessionStorage.getItem('usuarioGuardado') || 'null');
+
   // Variable que almacena la ausencia correspondiente
   idAusencia: number | null = null;
+
+  // Variable que almacena el profesor de guardia
+  profesorGuardia: string | null = null;
+
+  // Array de profesores para mostrar en el select
+    profesores: Profesor[] = [];
 
  // Variable para controlar la visualización del modal y el mensaje que muestra
  modalActivo: boolean = false;
@@ -25,6 +36,28 @@ export class TramosGuardiaComponent {
   cuartoTramo: false,
   horaCompleta: false
 };
+
+
+
+
+ constructor(
+    private profesorService: ProfesorService
+  ) { }
+
+
+  ngOnInit(): void {
+    this.profesorService.getProfesores().subscribe({
+      next: profesores => this.profesores = profesores,
+      error: err => console.error('Error al cargar profesores:', err)
+    });
+  }
+
+
+
+eliminarGuardia() {
+
+}
+
 
 
 
